c14b8e118066f0ba7178decd84e96e20c1efd8e7
Pedro Piva <pedro.piva@iterpec.com>
2024-06-28 16:28:14 -0300

    fix: add tabindex for safari

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+
+// https://github.com/rollup/rollup/issues/719
+// It is not working?
+// import replace from 'rollup-plugin-replace';
+
+import pkg from './package.json';
+
+export default {
+  input: './src/index.jsx',
+  output: {
+    file: pkg.embed,
+    format: 'umd',
+    name: 'myapp'
+  },
+  plugins: [
+    external(),
+    postcss(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+    // replace({
+    //  PRODUCTION: process.env.NODE_ENV === 'production'
+    // })
+  ]
+};
